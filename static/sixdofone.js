20cc53bea48a7e8276e0f19fab99c1639265c90b
David Laban <alsuren@gmail.com>
2024-08-15 11:34:36 +0100

    report start position; clean up some stuff

--- static/sixdofone.js
+++ static/sixdofone.js
@@ -21,9 +21,11 @@ let xrImmersiveRefSpace = null;
 let inlineViewerHelper = null;
 
 let isARAvailable = false;
-let isVRAvailable = false;
 /** @type XRSessionMode */
 let xrSessionString = 'immersive-vr';
+
+/** @type {XRViewerPose | "start" | undefined} */
+let dragStartPose = undefined;
 
 // WebGL scene globals.
 let gl = null;
@@ -37,12 +39,6 @@ scene.addNode(solarSystem);
 // Still adding a skybox, but only for the benefit of the inline view.
 let skybox = new SkyboxNode({ url: 'media/textures/milky-way-4k.png' });
 scene.addNode(skybox);
-
-const MAX_ANCHORED_OBJECTS = 30;
-let anchoredObjects = [];
-
-// Set with all anchors tracked in a previous frame.
-let all_previous_anchors = new Set();
 
 export function initXR() {
     xrButton = new WebXRButton({
@@ -60,12 +56,7 @@ export function initXR() {
         navigator.xr.isSessionSupported('immersive-ar').then((supported) => {
             isARAvailable = supported;
             xrButton.enabled = supported;
-            if (!supported) {
-                navigator.xr?.isSessionSupported('immersive-vr').then((supported) => {
-                    isVRAvailable = supported;
-                    xrButton.enabled = supported;
-                });
-            } else {
+            if (supported) {
                 xrSessionString = 'immersive-ar';
             }
         });
@@ -115,7 +106,8 @@ function initGL() {
  */
 function onSessionStarted(session) {
     session.addEventListener('end', onSessionEnded);
-    session.addEventListener('select', onSelect);
+    session.addEventListener('selectstart', onSelect);
+    session.addEventListener('selectend', onSelect)
 
     // @ts-expect-error anchors.html example squirrels away isImmersive on `session` : -(
     if (session.isImmersive && isARAvailable) {
@@ -147,11 +139,19 @@ function onSessionStarted(session) {
     });
 }
 
+
+/**
+ * @param { XRSession } session
+ */
 function onEndSession(session) {
     session.end();
 }
 
+/**
+ * @param { XRSessionEvent } event
+ */
 function onSessionEnded(event) {
+    // @ts-expect-error anchors.html example squirrels away isImmersive on `session` : -(
     if (event.session.isImmersive) {
         xrButton.setSession(null);
         // Turn the background back on when we go back to the in live view.
@@ -159,89 +159,32 @@ function onSessionEnded(event) {
     }
 }
 
-function addAnchoredObjectToScene(anchor) {
-    console.debug("Anchor created");
-
-    anchor.context = {};
-
-    let flower = new Gltf2Node({ url: 'media/gltf/sunflower/sunflower.gltf' });
-    scene.addNode(flower);
-    anchor.context.sceneObject = flower;
-    // @ts-expect-error anchors.html example squirrels away .anchor on `flower` : -(
-    flower.anchor = anchor;
-    anchoredObjects.push(flower);
-
-    // For performance reasons if we add too many objects start
-    // removing the oldest ones to keep the scene complexity
-    // from growing too much.
-    if (anchoredObjects.length > MAX_ANCHORED_OBJECTS) {
-        let objectToRemove = anchoredObjects.shift();
-        scene.removeNode(objectToRemove);
-        objectToRemove.anchor.delete();
-    }
-}
-
+/** @type {(this: XRSession, ev: XRInputSourceEvent) => any} */
 function onSelect(event) {
-    let frame = event.frame;
-    let session = frame.session;
-    let anchorPose = new XRRigidTransform();
-    let inputSource = event.inputSource;
-
-    // If the user is on a screen based device, place the anchor 1 meter in front of them.
-    // Otherwise place the anchor at the location of the input device
-    if (inputSource.targetRayMode == 'screen') {
-        anchorPose = new XRRigidTransform(
-            { x: 0, y: 0, z: -1 },
-            { x: 0, y: 0, z: 0, w: 1 });
+    if (event.type == 'selectstart') {
+        // We can't frame.getViewerPose() here because we're not in a requestAnimationFrame callback,
+        // so we set a sentinel and let it happen next time we are.
+        dragStartPose = "start";
     }
-
-    if (session.isImmersive) {
-        // Create a free-floating anchor.
-        frame.createAnchor(anchorPose, inputSource.targetRaySpace).then((anchor) => {
-            addAnchoredObjectToScene(anchor);
-        }, (error) => {
-            console.error("Could not create anchor: " + error);
-        });
+    else if (event.type == 'selectend') {
+        dragStartPose = undefined;
     }
 }
 
+/** @type XRFrameRequestCallback */
 // Called every time a XRSession requests that a new frame be drawn.
 function onXRFrame(t, frame) {
     let session = frame.session;
+    // @ts-expect-error anchors.html example squirrels away isImmersive on `session` : -(
     let xrRefSpace = session.isImmersive ?
         xrImmersiveRefSpace :
         inlineViewerHelper.referenceSpace;
     let pose = frame.getViewerPose(xrRefSpace);
 
-
-    reportPoseIfNeeded(t, pose);
-
-    // Update the position of all the anchored objects based on the currently reported positions of their anchors
-    const tracked_anchors = frame.trackedAnchors;
-    if (tracked_anchors) {
-        all_previous_anchors.forEach(anchor => {
-            if (!tracked_anchors.has(anchor)) {
-                scene.removeNode(anchor.sceneObject);
-            }
-        });
-
-        tracked_anchors.forEach(anchor => {
-            const anchorPose = frame.getPose(anchor.anchorSpace, xrRefSpace);
-            if (anchorPose) {
-                anchor.context.sceneObject.matrix = anchorPose.transform.matrix;
-                anchor.context.sceneObject.visible = true;
-            } else {
-                anchor.context.sceneObject.visible = false;
-            }
-        });
-
-        all_previous_anchors = tracked_anchors;
-    } else {
-        all_previous_anchors.forEach(anchor => {
-            scene.removeNode(anchor.sceneObject);
-        });
-
-        all_previous_anchors = new Set();
+    // @ts-expect-error this isImmersive is my fault. I think I might just make a single god object
+    // and pass all of my state around using that rather than shoving it on `session`?
+    if (session.isImmersive) {
+        reportPoseIfNeeded(t, pose);
     }
 
     // In this sample and most samples after it we'll use a helper function
@@ -265,6 +208,10 @@ function onXRFrame(t, frame) {
 let lastSendTime = performance.now();
 const sendInterval = 100; // Time in milliseconds
 
+/**
+ * @param {number} t
+ * @param {XRViewerPose | undefined} pose
+ */
 function reportPoseIfNeeded(t, pose) {
     if (pose && (t - lastSendTime > sendInterval)) {
         sendData(pose);
@@ -272,7 +219,16 @@ function reportPoseIfNeeded(t, pose) {
     }
 }
 
+/**
+ * @param { XRViewerPose } pose
+ */
 function sendData(pose) {
+    if (dragStartPose == "start") {
+        dragStartPose = pose;
+        // recurse to make it easier for typescript to reason about the types below
+        return sendData(pose)
+    }
+
     fetch('/api/report', {
         method: 'POST',
         headers: {
@@ -280,7 +236,9 @@ function sendData(pose) {
         },
         body: JSON.stringify({
             position: { x: pose.transform.position.x, y: pose.transform.position.y, z: pose.transform.position.z },
-            orientation: { x: pose.transform.orientation.x, y: pose.transform.orientation.y, z: pose.transform.orientation.z, w: pose.transform.orientation.w }
+            orientation: { x: pose.transform.orientation.x, y: pose.transform.orientation.y, z: pose.transform.orientation.z, w: pose.transform.orientation.w },
+            dragStartPosition: dragStartPose && { x: dragStartPose.transform.position.x, y: dragStartPose.transform.position.y, z: dragStartPose.transform.position.z },
+            dragStartOrientation: dragStartPose && { x: dragStartPose.transform.orientation.x, y: dragStartPose.transform.orientation.y, z: dragStartPose.transform.orientation.z, w: dragStartPose.transform.orientation.w },
         })
     }).then(response => response.json())
         .then(data => console.log('Pose data sent successfully'))
